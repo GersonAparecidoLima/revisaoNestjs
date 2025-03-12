6784368e60ad8452e72c5149077e04ec3f69117a
Gerson Aparecido <gerson.lima@yduqs.com.br>
2025-03-12 10:28:05 -0300

    nestjs-criando-api-resftul - Segunda parte

--- src/usuario/usuario.repository.ts
+++ src/usuario/usuario.repository.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { UsuarioEntity } from './usuario.entity';
 
 @Injectable()
 //vamos isolar a lógica para salvar dados do usuário
 export class UsuarioRepository {
         //criando uma variavel com um array vazio
-    private usuarios = [];
+        private usuarios: UsuarioEntity[] = [];
 
-    async salvar(usuario) {
+    async salvar(usuario: UsuarioEntity) {
         this.usuarios.push(usuario);
     }
 
@@ -23,5 +24,41 @@ export class UsuarioRepository {
         return possivelUsuario !== undefined;
     }
 
+    private buscaPorId(id: string) {
+        const possivelUsuario = this.usuarios.find(
+            usuarioSalvo => usuarioSalvo.id === id
+        );
+    
+        if(!possivelUsuario) {
+            throw new Error('Usuário não existe');
+        }
+    
+        return possivelUsuario;
+    }
+
+
+    async atualiza(id: string, dadosDeAtualizacao: Partial<UsuarioEntity>) {
+        const usuario = this.buscaPorId(id);
+    
+        Object.entries(dadosDeAtualizacao).forEach(([chave, valor]) => {
+            if (chave === 'id') {
+                return;
+            }
+    
+            usuario[chave] = valor;
+        });
+    
+        return usuario;
+    }
+
+
+    async remove(id: string){
+        const usuario = this.buscaPorId(id);
+        this.usuarios = this.usuarios.filter(
+            usuarioSalvo => usuarioSalvo.id !== id
+        );
+    
+        return usuario;
+    }
 }
 
